ddb199444b24260b7bd4255625937661eb3268e7
Garrison <garrison.jabs@gmail.com>
2023-01-29 23:13:27 -0700

    Added a button component and button pressed control script

---
+++ Assets/MyScripts/ButtonEffect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+public class ButtonEffect : MonoBehaviour
+{
+    public void OnButtonDown(Hand fromHand)
+    {
+        ColorSelf(Color.cyan);
+        fromHand.TriggerHapticPulse(1000);
+    }
+
+    public void OnButtonUp(Hand fromHand)
+    {
+        ColorSelf(Color.red);
+    }
+
+    private void ColorSelf(Color newColor)
+    {
+        Renderer[] renderers = this.GetComponentsInChildren<Renderer>();
+        for (int rendererIndex = 0; rendererIndex < renderers.Length; rendererIndex++)
+        {
+            renderers[rendererIndex].material.color = newColor;
+        }
+    }
+}
