23454af21583faf780b94a76e0637461922e617b
Garrison <garrison.jabs@gmail.com>
2023-02-27 23:26:22 -0700

    generated a light map for the museam scene and logic for the start menu UI

--- Assets/MyScripts/LevelSwitch.cs
+++ Assets/MyScripts/LevelSwitch.cs
@@ -6,32 +6,25 @@ using Valve.VR;
 public class LevelSwitch : MonoBehaviour
 {
     public int currLevel = 0;
-
-    public string[] levelNames = new string[2] { "ExperimentalScene", "museum" };
-
+    
+    public string[] levelNames = new string[3] {"StartMenu" ,"ExperimentalScene", "museum" };
+    private int NoScenes = 0;
     static LevelSwitch Switch = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        NoScenes = levelNames.Length;
         if (Switch == null) Switch = this;
         else Destroy(this.gameObject);
 
         DontDestroyOnLoad(this.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.anyKeyDown)
-        {
-            currLevel = (currLevel + 1) % 2;
-            SteamVR_LoadLevel.Begin(levelNames[currLevel]);
-        }
-    }
     public void loadLevel()
     {
-        currLevel = (currLevel + 1) % 2;
+        Debug.Log(NoScenes);
+        currLevel = (currLevel + 1) % NoScenes;
         SteamVR_LoadLevel.Begin(levelNames[currLevel]);
     }
 }
